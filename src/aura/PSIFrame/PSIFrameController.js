09f0ada8eb3a67fb633151a83d65e0cc2a6ec930
Troy Hedges <thedges@salesforce.com>
2018-11-16 14:28:48 -0600

    Initial load

---
+++ src/aura/PSIFrame/PSIFrameController.js
@@ -0,0 +1,6 @@
+({
+	adjustIframe : function(component, event, helper) {
+		console.log("adjust iframe");
+        //document.getElementById("form-iframe").style.height = document.getElementById("form-iframe").contentWindow.document.body.scrollHeight + "px";
+	}
+})
